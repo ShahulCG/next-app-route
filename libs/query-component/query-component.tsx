0f0fb1c3658b3335ae5a0a91d3d3eaf8c18272f8
ShahulCG <shahul431@gmail.com>
2025-11-28 11:50:46 +0530

    changes

--- libs/query-component/query-component.tsx
+++ libs/query-component/query-component.tsx
@@ -4,7 +4,7 @@
     import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
     import { useState } from 'react';
 
-    export default function ReactQueryClientProvider({ children }) {
+    export default function ReactQueryClientProvider({ children }: any) {
       const [queryClient] = useState(() => new QueryClient());
       return (
         <QueryClientProvider client={queryClient}>
